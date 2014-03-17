3bac538239382cfd9c38fccede2a4c3c69cc2baa
Ryan Kendall <ryankendallt@gmail.com>
2014-03-17 17:29:30 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jogger::Application.config.secret_key_base = 'aee8aa11fe20371bc9f43c55f4ef721940535cb1f6247faaaa1f1eae55246a13f90ac3f6222cd3ce59258ba23346c32a63a652c4c00fa46577a57761bc8000dc'
