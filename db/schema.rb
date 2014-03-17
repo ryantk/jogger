5d7f8654a60bc5a931c0786fe57f19569f3ebcff
Ryan Kendall <ryankendallt@gmail.com>
2014-03-17 19:46:28 +0000

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317194245) do
+ActiveRecord::Schema.define(version: 20140317194539) do
+
+  create_table "comments", force: true do |t|
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "email",                  default: "", null: false
