5d8e412612457860c2246596f743caddcf0b50b3
Ryan Kendall <ryankendallt@gmail.com>
2014-03-18 16:55:11 +0000

    some specs in journal entry

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe JournalEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:entry) { build(:journal_entry) }
+
+  specify { expect(entry).to respond_to(:body) }
+  specify { expect(entry).to respond_to(:body=) }
+  specify { expect(entry).to respond_to(:journal) }
+  specify { expect(entry).to respond_to(:tags) }
+
+  it "can add body" do
+    body = "a long entry"
+    entry.body = body
+    expect(entry.body).to eq(body)
+  end
+
+  it "can add tags" do
+    entry.tags << build(:tag)
+    expect(entry.tags.length).to be(1)
+  end
+
 end
